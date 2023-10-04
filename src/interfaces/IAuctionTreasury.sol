427b0aacef2fb43d776f72b2072dbe6842389529
xenafi <xenafi.dev@gmail.com>
2023-10-04 11:53:10 +0800

    Removed unused interfaces

--- src/interfaces/IAuctionTreasury.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.18;
-
-interface IAuctionTreasury {
-    function transferXEN(address _to, uint256 _amount) external;
-    function distribute() external;
-}
--- src/interfaces/IReferralRegistry.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.18;
-
-interface IReferralRegistry {
-    function referredBy(address) external view returns (address);
-
-    function setReferrer(address _trader, address _referrer) external;
-}
